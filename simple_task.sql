86241a047705ca1378a71f01a75d0ee725ea1ccf
ReshmaMurali24 <166366248+ReshmaMurali24@users.noreply.github.com>
2024-04-08 13:09:54 +0530

    Update simple_task.sql

--- simple_task.sql
+++ simple_task.sql
@@ -1,5 +1,5 @@
 CREATE TASK IF NOT EXISTS simple_task
-    WAREHOUSE = 'xsmall'
+    WAREHOUSE = 'compute_wh'
     SCHEDULE = 'USING CRON 0 * * * * UTC'
 AS
     CALL simple_procedure('This is from a task');
