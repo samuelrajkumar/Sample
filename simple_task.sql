38958f59be0dd0fbf203e832de9e4719d88c2a01
ReshmaMurali24 <166366248+ReshmaMurali24@users.noreply.github.com>
2024-04-10 10:56:02 +0530

    Update simple_task.sql

--- simple_task.sql
+++ simple_task.sql
@@ -2,4 +2,4 @@ CREATE TASK IF NOT EXISTS simple_task
     WAREHOUSE = 'compute_wh'
     SCHEDULE = 'USING CRON 0 * * * * UTC'
 AS
-    CALL simple_procedure('This is from a task');
+    CALL populatedata();
