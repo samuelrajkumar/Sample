e616d820774e82cf8905b369445765636326fb5a
ReshmaMurali24 <166366248+ReshmaMurali24@users.noreply.github.com>
2024-04-18 12:04:46 +0530

    Update always_calculate_discount.sql

--- always_calculate_discount.sql
+++ always_calculate_discount.sql
@@ -1,13 +1,7 @@
--- always_calculate_discount.sql
--- Description: Ensure the calculate_discount UDF always exists
-
-CREATE OR REPLACE FUNCTION git_demo.public.calculate_discount(price Number, discount_percent Number)
-RETURNS NUMBER
-LANGUAGE SQL
-EXECUTE AS CALLER
-AS
-$$
-BEGIN
-return price * (1 - discount_percent / 100);
-END;
-$$
+CREATE OR REPLACE FUNCTION git_demo.public.calculate_discount(price float, discount_percent number)
+RETURNS FLOAT 
+  AS
+  $$
+     price * (1 - discount_percent / 100)
+  $$
+  ;
