3bc053c3d18b971ec9ff8ed03425a1a213ce308e
ReshmaMurali24 <166366248+ReshmaMurali24@users.noreply.github.com>
2024-04-18 10:58:26 +0530

    Update always_calculate_discount.sql

--- always_calculate_discount.sql
+++ always_calculate_discount.sql
@@ -3,10 +3,11 @@
 
 CREATE OR REPLACE FUNCTION git_demo.public.calculate_discount(price Number, discount_percent Number)
 RETURNS NUMBER
-LANGUAGE JAVASCRIPT
+LANGUAGE SQL
 EXECUTE AS CALLER
 AS
 $$
-// JavaScript logic for calculating the discount
+BEGIN
 return price * (1 - discount_percent / 100);
-$$;
+END;
+$$
